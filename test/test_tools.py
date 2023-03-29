947d2541b18bda00e9390c8e092d3bbbf86926a0
Mara-185 <marastefania.calo@gmail.com>
2023-03-29 19:05:28 +0200

    Travis test

--- test/test_tools.py
+++ test/test_tools.py
@@ -64,7 +64,7 @@ class ZAsymmetryTest(unittest.TestCase):
 
         logger.info("Test on \"dilepton_vec\" function...")
         pt, eta, phi, mass, charge = create_example()
-        dilepton = ROOT.dilepton_vec(pt[0], eta[0], phi[0], mass[0], pt[1], \
+        dilepton = ROOT.tools_cpp.dilepton_vec(pt[0], eta[0], phi[0], mass[0], pt[1], \
             eta[1], phi[1], mass[1])
         self.assertAlmostEqual(dilepton[0], 34.4752, 4)
         self.assertAlmostEqual(dilepton[1], 2.87066, 4)
